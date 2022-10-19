74e0845862773fe1d9f732de31eee8486add6b63
MattiasSpang <spangberg.mattias@gmail.com>
2022-10-19 09:26:35 +0200

    Initial Commit

---
+++ ios_test_assistent/ios_test_assistentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_test_assistentApp.swift
+//  ios_test_assistent
+//
+//  Created by Mattias Spångberg on 2022-10-19.
+//
+
+import SwiftUI
+
+@main
+struct ios_test_assistentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
