4e3f933f8b6d4b374b264a1c825ae52bbeef3653
MattiasSpang <spangberg.mattias@gmail.com>
2022-10-19 15:38:04 +0200

    started a todo app

--- ios_test_assistent/ContentView.swift
+++ ios_test_assistent/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+class TodoItem: Identifiable{
+    
+    @State var name: String
+    @State var checked: Bool
+    
+    init(name: String, checked: Bool) {
+        self.name = name
+        self.checked = checked
+    }
+    
+    func getName() -> String{
+        return self.name
+    }
+    
+    func getChecked() -> Bool{
+        return self.checked
+    }
+    
+    func toggleChecked(){
+        self.checked.toggle()
+    }
+    
+}
+
 struct ContentView: View {
+    var menuName: String = "Menu"
+    @State var todoArray: [TodoItem] = []
+    @State var iconTapped: Bool = false
+    @State var todoText: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(){
+                ForEach(todoArray){ item in
+                    HStack{
+                        Text(item.name)
+                        Spacer()
+                        if(iconTapped){
+                            Image(systemName: "checkmark.circle")
+                        }else{
+                            Image(systemName: "circle")
+                        }
+                    }
+                }
+            }
+            .onTapGesture {
+                iconTapped.toggle()
+            }
+            .foregroundColor(Color.pink)
+            Spacer()
+            TextField("Enter Todo item: ", text: $todoText)
+                .onSubmit {
+                    let todoitem = TodoItem(name: todoText, checked: false)
+                    todoArray.append(todoitem)
+                    todoText = ""
+                }
+                .padding()
         }
-        .padding()
     }
 }
 
